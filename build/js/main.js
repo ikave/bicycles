67fda0221cc6c98e74888566d0f5d36405259f6a
ikave <shiftgear@mail.ru>
2021-04-06 09:20:17 +0300

    Исправляет замечания наставника

--- build/js/main.js
+++ build/js/main.js
@@ -2,22 +2,24 @@
 
 (function(){
   var pageHeader = document.querySelector('.page-header');
-  var menu = pageHeader.querySelector('.page-header__menu');
-  var headerToggle = pageHeader.querySelector('.page-header__toggle');
+  var headerToggle = document.querySelector('.page-header__toggle');
+  var menu = document.querySelector('.page-header__menu');
 
-  pageHeader.classList.remove('page-header--nojs');
+  if (pageHeader && headerToggle) {
+    pageHeader.classList.remove('page-header--nojs');
 
-  headerToggle.addEventListener('click', function () {
-    headerToggle.classList.toggle('page-header__toggle--opened');
+    headerToggle.addEventListener('click', function () {
+      headerToggle.classList.toggle('page-header__toggle--opened');
 
-    if (menu.classList.contains('menu--closed')) {
-      menu.classList.remove('menu--closed');
-      menu.classList.add('menu--opened');
-      document.body.style.overflowY = 'hidden';
-    } else {
-      menu.classList.add('menu--closed');
-      menu.classList.remove('menu--opened');
-      document.body.style.overflowY = 'visible';
-    }
-  });
+      if (menu.classList.contains('menu--closed')) {
+        menu.classList.remove('menu--closed');
+        menu.classList.add('menu--opened');
+        document.body.style.overflowY = 'hidden';
+      } else {
+        menu.classList.add('menu--closed');
+        menu.classList.remove('menu--opened');
+        document.body.style.overflowY = 'visible';
+      }
+    });
+  }
 })();
